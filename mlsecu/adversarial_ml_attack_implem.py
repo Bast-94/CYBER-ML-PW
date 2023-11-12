da032d769ac718a747a0eee926aec2090f90b851
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-11-12 15:21:41 +0000

    FIX(fgsm): data_point update.

--- mlsecu/adversarial_ml_attack_implem.py
+++ mlsecu/adversarial_ml_attack_implem.py
@@ -1,4 +1,20 @@
 import numpy as np
+# Test fgsm_attack_svm_2c
+from sklearn import svm
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+# import makeblobs
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
 def fgsm_attack_svm_2c(classifier, orig_point, dist_function, step=None, epsilon=np.inf, max_step=200):
     data_point = orig_point.copy()
     orig_class = classifier.predict(data_point.reshape(1, -1))[0]
@@ -6,16 +22,20 @@ def fgsm_attack_svm_2c(classifier, orig_point, dist_function, step=None, epsilon
     current_eps = dist_function(data_point,orig_point)
     attack_info = {}
     if step is None:
-        step = 0
+        step = 0.01
+    i = 0
+    
+    eps_evol = [current_eps]
     while orig_class == new_class:
-        if current_eps < epsilon and step < max_step:
+        if current_eps < epsilon and i < max_step:
             # get datapoint by gradient descent
             grad = classifier.coef_[0]
-            data_point = data_point + grad 
+            data_point = data_point + step * np.sign(grad)
                         
             new_class = classifier.predict(data_point.reshape(1, -1))[0]
             current_eps = dist_function(data_point,orig_point)
             attack_info = dict(data_point=data_point, current_eps=current_eps)
+            eps_evol.append(current_eps)
         else:
             attack_info = dict(data_point=None, current_eps=None)
             if current_eps > epsilon:
@@ -23,26 +43,18 @@ def fgsm_attack_svm_2c(classifier, orig_point, dist_function, step=None, epsilon
             if step >= max_step:
                 print("Attack failed: max step exceeded")
             break
-        step += 1
+        i += step
+    fig, ax = plt.subplots()
+    ax.plot(eps_evol)
+    ax.set_xlabel("Step")
+    ax.set_ylabel("Distance")
+    ax.set_title("Evolution of distance to original point")
+    plt.show()
+    fig.savefig("eps_evol.png")
     return attack_info.values()
 
 if __name__ == "__main__":
-    # Test fgsm_attack_svm_2c
-    from sklearn import svm
-    from sklearn import datasets
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import accuracy_score
-    from sklearn.metrics import confusion_matrix
-    from sklearn.metrics import classification_report
-    from sklearn.metrics import roc_auc_score
-    from sklearn.metrics import roc_curve
-    # import makeblobs
-    from sklearn.datasets import make_blobs
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    import pandas as pd
-    import numpy as np
-    import os
+    
     
     
     X , y = make_blobs(n_samples=1000, centers=2, n_features=2, random_state=0)
@@ -60,6 +72,6 @@ if __name__ == "__main__":
     
     # Attack
     dist_func = lambda x, y: np.linalg.norm(x - y)
-    print("SVM coef:", clf.coef_)
-    attack_info = fgsm_attack_svm_2c(clf, X_test[0], dist_function=dist_func, epsilon=0.1)
+    print("SVM coef:", clf.coef_[0])
+    attack_info = fgsm_attack_svm_2c(clf, X_test[0], dist_function=dist_func)
     print("Attack info:", attack_info)
